49403c20b8458914b35a039c81f773f70b7b9391
Murugesan Selvakumar-XMVT36 <XMVT36@motorola.com>
2025-02-10 12:16:50 -0800

    add new changes-2

--- src/main/java/com/devops/springselenium/TestWithMain.java
+++ src/main/java/com/devops/springselenium/TestWithMain.java
@@ -36,7 +36,13 @@ public class TestWithMain {
 */
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--disable-gpu", "--no-sandbox");
+        //options.addArguments("--headless", "--disable-gpu", "--no-sandbox");
+        options.addArguments("--headless");  // Run in headless mode
+        options.addArguments("--disable-gpu"); // Needed for headless mode
+        options.addArguments("--no-sandbox"); // Bypass OS security model (important for AWS)
+        options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource issues in AWS
+        options.addArguments("--remote-allow-origins=*"); // Required for ChromeDriver 111+
+
         driver = new ChromeDriver(options);
 
         //driver = new ChromeDriver();
